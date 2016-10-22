9fda3f85174efef622734057aba3a3f50e7af123
Sitara C Persad <spersad@w20-575-mac-03.mit.edu>
2016-10-22 17:45:04 -0400

    added some halfassed code to Prob 2

--- hw2_resources/Problem1.py
+++ hw2_resources/Problem1.py
@@ -1,11 +1,6 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Oct 22 00:52:43 2016
-
-@author: Sitara
-"""
 
 #PROBLEM 1: Logistic Regression (LR)
+
 import sklearn.linear_model as linear
 
 #PART A: Gradient Descent
@@ -14,10 +9,10 @@ import sklearn.linear_model as linear
  with L2 regularization on the weight vector
  '''
  
- # Can we use the modified gradient descent we 
- # made in HW1
+# GET SGD FROM HW1
+
  def gradient_descent(data, L, max_epochs):
-     pass
+	 pass
  
  
 #PART B: Comparing Regularization
@@ -34,22 +29,23 @@ QUESTION: Doesn't the weight define the decision boundary what does it MEANNNNNN
 
 sklearn: 
 LogisticRegression(penalty='l2', dual=False, tol=0.0001, C=1.0, 
-                   fit_intercept=True, intercept_scaling=1, 
-                   class_weight=None, random_state=None, solver='liblinear', 
-                   max_iter=100, multi_class='ovr', verbose=0, 
-                   warm_start=False, n_jobs=1)
-                   
+				   fit_intercept=True, intercept_scaling=1, 
+				   class_weight=None, random_state=None, solver='liblinear', 
+				   max_iter=100, multi_class='ovr', verbose=0, 
+				   warm_start=False, n_jobs=1)
+				   
 http://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html
 '''
 
 def compare_logit(data, L):
-    ''' Accepts a dataset and lambda value, L.
-        Computes the weight vectors in the case of L1 & L2 regression.
-        
-        Plots the weight values for L1 and L2 for a given L.
-        Outputs the classification error in the L1 and L2 case.
-        
-    '''
-    
-    
-    
+	''' 
+	Accepts a dataset and lambda value, L.
+	Computes the weight vectors in the case of L1 & L2 regression.
+	
+	Plots the weight values for L1 and L2 for a given L.
+	Outputs the classification error in the L1 and L2 case.
+		
+	'''
+	
+	
+	
--- hw2_resources/Problem2.py
+++ hw2_resources/Problem2.py
@@ -1,55 +1,40 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Oct 22 00:52:43 2016
+#PROBLEM 2: Logistic Regression (LR)
 
-@author: Sitara
-"""
+import numpy as np
+import cvxopt as opt 
 
-#PROBLEM 1: Logistic Regression (LR)
-import sklearn.linear_model as linear
-
-#PART A: Gradient Descent
+#PART A: Dual Form of Linear SVMs
 '''
- Use a gradient descent method to optimize the logistic regression objective,
- with L2 regularization on the weight vector
- '''
- 
- # Can we use the modified gradient descent we 
- # made in HW1
- def gradient_descent(data, L, max_epochs):
-     pass
- 
- 
-#PART B: Comparing Regularization
-'''
-Let’s now compare the effects of L1 and L2 regularization on LR. 
-Minimize NLL + Lambda(Lx Norm).
-
-Evaluate the effect of the choice of regularizer (L1 vs L2) and 
-the value of λ on (a) the weights, (b) the decision boundary and (c)
- the classiﬁcation error rate in each of the training data sets
-
-
-QUESTION: Doesn't the weight define the decision boundary what does it MEANNNNNN
-
-sklearn: 
-LogisticRegression(penalty='l2', dual=False, tol=0.0001, C=1.0, 
-                   fit_intercept=True, intercept_scaling=1, 
-                   class_weight=None, random_state=None, solver='liblinear', 
-                   max_iter=100, multi_class='ovr', verbose=0, 
-                   warm_start=False, n_jobs=1)
-                   
-http://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html
+Implement the dual form of linear SVMs with slack variables. Please do not use the built-in SVM 
+implementation in Matlab or sklearn. Instead, write a program that takes data as input, converts
+it to the appropriate objective function and constraints, and then calls a quadratic programming
+package to solve it. See the file optimizers.txt for installation and usage for matlab/python.
 '''
 
-def compare_logit(data, L):
-    ''' Accepts a dataset and lambda value, L.
-        Computes the weight vectors in the case of L1 & L2 regression.
-        
-        Plots the weight values for L1 and L2 for a given L.
-        Outputs the classification error in the L1 and L2 case.
-        
-    '''
-    
-    
-    
+# JUST IMPLEMENT KERNEL VERSION AND USE phi(x) = x for PART A 
+
+
+
+
+#PART B: Test Your Dual Form of Linear SVMs
+'''
+Implement the dual form of linear SVMs with slack variables. Please do not use the built-in SVM 
+implementation in Matlab or sklearn. Instead, write a program that takes data as input, converts
+it to the appropriate objective function and constraints, and then calls a quadratic programming
+package to solve it. See the file optimizers.txt for installation and usage for matlab/python.
+'''
+
+
+
+
+ 
+#PART C: Test Your Dual Form of Linear SVMs
+'''
+he dual form SVM is useful for several reasons, including an ability to handle kernel functions 
+that are hard to express as feature functions in the primal form. Extend your dual form SVM code 
+to operate with kernels. Do the implementation as generally as possible, so that it either takes
+ the kernel function or kernel matrix as input.
+'''
+
+
+
